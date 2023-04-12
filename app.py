0860894816bc64ad00a20dad6d920578e06994ac
LuiseStrathe <luise.strathe@gmail.com>
2023-04-12 21:13:04 +0200

    Prototype
    Working local , to be transferred onto server

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from secret import secret_key
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = secret_key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gamebook.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+import routes
+
+if __name__ == "__main__":
+	app.run()
